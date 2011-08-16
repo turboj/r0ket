adf77356e6bfb54e53ddc4c95f127492040439b7
Turbo J <turboj@web.de>
2011-08-17 00:32:43 +0200

    Simple Soft-PWM for slow start of external Power. Fixes Geiger Counter startup issues - the 100uF cap drains VCC caps too much.

--- firmware/basic/basic.c
+++ firmware/basic/basic.c
@@ -12,6 +12,13 @@ void rbInit() {
     // TODO FIXME more power init needed ? chrg + volt input ?
     // enable external vcc
     gpioSetDir(RB_PWR_GOOD, gpioDirection_Output);
+    for (int i=0; i< 100; i++){
+
+    	for (int j=0; j<100; j++){
+    		 if (j>i) gpioSetValue (RB_PWR_GOOD, 1); else
+    			 gpioSetValue (RB_PWR_GOOD, 0);
+    	}
+    }
     gpioSetValue (RB_PWR_GOOD, 0); 
 
     // Disable USB Connect (we don't want USB by default)
